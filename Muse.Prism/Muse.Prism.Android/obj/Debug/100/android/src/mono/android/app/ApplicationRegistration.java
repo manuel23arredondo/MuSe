5df24ba713140e0bcbda70adf2005bb4ae011e6f
Manuel <arredondo23manuel@gmail.com>
2022-09-08 17:05:56 -0500

    Pantalla de inicio

---
+++ Muse.Prism/Muse.Prism.Android/obj/Debug/100/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Muse.Prism.Droid.MainApplication, Muse.Prism.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc647ef5177c3589dbc6.MainApplication.class, crc647ef5177c3589dbc6.MainApplication.__md_methods);
+		
+	}
+}
