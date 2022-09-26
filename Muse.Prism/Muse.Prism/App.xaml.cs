9b7a26cfb4b6ae967e767e01bf674085f0254fcd
Manuel <arredondo23manuel@gmail.com>
2022-09-26 12:43:42 -0500

    Cambios en diseño

--- Muse.Prism/Muse.Prism/App.xaml.cs
+++ Muse.Prism/Muse.Prism/App.xaml.cs
@@ -19,7 +19,7 @@ namespace Muse.Prism
         {
             InitializeComponent();
 
-            await NavigationService.NavigateAsync("NavigationPage/MonitoreadasPage");
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
